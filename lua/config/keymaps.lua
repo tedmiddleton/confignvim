90376197ac0e44c70641cba3993979f342fabe64
Ted Middleton <middleton.ted@gmail.com>
2025-03-13 10:27:13 -0700

    Gradually modifying and customizing lazyvim.
    - need , rather than space as leader
    - df <-> <Esc>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_set_keymap("n", "df", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "df", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "df", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("c", "df", "<Esc>", { noremap = true, silent = true })
