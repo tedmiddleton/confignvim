20cdfef6137cb5fd0bacd89bab9e86ee1ba9e99b
Ted Middleton <middleton.ted@gmail.com>
2025-03-17 21:07:24 -0700

    Copilot default model claude-3.7-sonnet

--- lua/plugins/customizations.lua
+++ lua/plugins/customizations.lua
@@ -5,6 +5,22 @@ if lazyvim_docs then
 end
 
 return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = function()
+      local user = vim.env.USER or "User"
+      user = user:sub(1, 1):upper() .. user:sub(2)
+      return {
+        auto_insert_mode = true,
+        question_header = "  " .. user .. " ",
+        answer_header = "  Copilot ",
+        model = "claude-3.7-sonnet",
+        window = {
+          width = 0.4,
+        },
+      }
+    end,
+  },
 
   {
     "folke/snacks.nvim",
