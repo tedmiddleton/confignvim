bbced617aa0b8b2b07ea2c20c731b47fd78dc9e2
Ted Middleton <ted.middleton@icapital.com>
2025-04-08 15:22:05 -0700

    Changes
    - Disabling noice because it messes with makeprg
    - It turns out that if you want the indent guides disabled by default
      but still available with <leader>ug, you need to add indent =
      {enabled=false} to snacks.nvim
    - Fix for copilot error

--- lua/plugins/customizations.lua
+++ lua/plugins/customizations.lua
@@ -21,13 +21,31 @@ return {
       }
     end,
   },
-
+  {
+    "folke/noice.nvim",
+    enabled = false,
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
   {
     "folke/snacks.nvim",
     opts = {
       scroll = {
         enabled = false, -- Disable scroll animation
       },
+      indent = {
+        enabled = false,
+      }
     },
 
     keys = {
@@ -91,5 +109,12 @@ return {
         desc = "Explorer NeoTree (Root Dir)",
       },
     },
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    optional = true,
+    opts = function()
+      require("copilot.api").status = require("copilot.status")
+    end,
   },
 }
