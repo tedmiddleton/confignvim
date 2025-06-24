0fd885ca8610ad2c975ae39cc5ac2b25c6e83a17
Ted Middleton <middleton.ted@gmail.com>
2025-06-24 13:58:39 -0700

    Adding an ollama config for Avante

--- lua/plugins/customizations.lua
+++ lua/plugins/customizations.lua
@@ -204,8 +204,18 @@ return {
     opts = {
       -- add any opts here
       -- for example
-      provider = "claude",
+      --provider = "claude",
+      provider = "ollama",
       providers = {
+        ollama = {
+          endpoint = "http://localhost:11434",
+          model = "qwen3:14B",
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 20480,
+          },
+        },
         claude = {
           endpoint = "https://api.anthropic.com",
           model = "claude-sonnet-4-20250514",
